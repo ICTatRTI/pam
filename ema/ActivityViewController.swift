ed86965227c270e61a64fb95a075fd71ff39b6c1
Adam Preston <adam.m.preston@gmail.com>
2018-03-19 11:53:54 -0600

    More specific name

--- ema/ActivityViewController.swift
+++ ema/ActivityViewController.swift
@@ -24,7 +24,7 @@ enum Activity: Int {
     var title: String {
         switch self {
         case .PAMSurvey:
-            return "PAM Survey"
+            return "Impacts of Lighting Survey"
 
         }
     }
